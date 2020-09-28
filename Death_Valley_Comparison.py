17ccaa57dabb39ea493af3f5ec902146f35e7fdc
Cope <Mark_Cope1@baylor.edu>
2020-09-27 21:19:34 -0500

    Changes to comp

--- Death_Valley_Comparison.py
+++ Death_Valley_Comparison.py
@@ -5,8 +5,8 @@ open_death= open("death_valley_2018_simple.csv", "r")
 open_sitka= open("sitka_weather_2018_simple.csv", "r")
 
 
-csv_d = csv.reader(open_file, delimiter= ",")
-csv_s = csv.reader(open_file, delimiter= ",")
+csv_d = csv.reader(open_death, delimiter= ",")
+csv_s = csv.reader(open_sitka, delimiter= ",")
 
 header_row = next(csv_d)
 header_row = next(csv_s)
@@ -28,29 +28,49 @@ for row in csv_d:
         current_dateD = datetime.strptime(row[2],"%Y-%m-%d") 
         
     except ValueError:
-        print(f"Missing data for {current_date}")
+        print(f"Missing data for {current_dateD}")
     else:
         highsD.append(highD)
         lowsD.append(lowD)
         datesD.append(current_dateD)
     
+for row in csv_s:
+    try:
+        highS = int(row[5])
+        lowS = int(row[6])
+        current_dateS = datetime.strptime(row[2],"%Y-%m-%d") 
+        
+    except ValueError:
+        print(f"Missing data for {current_dateS}")
+    else:
+        highsS.append(highS)
+        lowsS.append(lowS)
+        datesS.append(current_dateS)
+    
 
-print(highs)
+print(highsD)
+print(highsS)
 
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
+fig, ax = plt.subplots(2,2)
 
 
-plt.plot(dates, highs, c="red", alpha= 0.5)
-plt.plot(dates, lows, c="blue", alpha= 0.5)
+ax[0].plot(datesD, highD, c="red", alpha=0.5)
+ax[0].plot(datesD, highD, c="blue", alpha=0.5)
+ax[1].plot(datesS, highS, c="red", alpha=0.5)
+ax[1].plot(datesS, highS, c="blue", alpha=0.5)
 
-plt.title("Daily High Temp, July 2018", fontsize=16)
+
+
+plt.title("Daily High and Low Temp- 2018 and Death Valley", fontsize=16)
 plt.xlabel("")
+
+plt.fill_between(datesD, highsD, lowsD, facecolor= 'blue', alpha=0.1)
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis="both", labelsize=16)
 
-plt.fill_between(dates, highs, lows, facecolor= "blue",alpha= 0.1)
+
 
 fig.autofmt_xdate()
 
