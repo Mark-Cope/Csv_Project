d31b222595defc0324bbd41e2ba142e15151482d
Cope <Mark_Cope1@baylor.edu>
2020-09-27 20:39:52 -0500

    Started changes on Code

--- Death_Valley_Comparison.py
+++ Death_Valley_Comparison.py
@@ -1,32 +1,38 @@
-import csv #Double everything with new names
+import csv
 from datetime import datetime
 
-open_file= open("death_valley_2018_simple.csv", "r")
-
-csv_file = csv.reader(open_file, delimiter= ",")
-
-header_row = next(csv_file)
+open_death= open("death_valley_2018_simple.csv", "r")
+open_sitka= open("sitka_weather_2018_simple.csv", "r")
 
 
+csv_d = csv.reader(open_file, delimiter= ",")
+csv_s = csv.reader(open_file, delimiter= ",")
 
-highs = []
-dates = []
-lows = []
+header_row = next(csv_d)
+header_row = next(csv_s)
+
+highsD = []
+datesD = []
+lowsD = []
+
+highsS = []
+datesS = []
+lowsS = []
 
 
 
-for row in csv_file:
+for row in csv_d:
     try:
-        high = int(row[4])
-        low = int(row[5])
-        current_date = datetime.strptime(row[2],"%Y-%m-%d") 
+        highD = int(row[4])
+        lowD = int(row[5])
+        current_dateD = datetime.strptime(row[2],"%Y-%m-%d") 
         
     except ValueError:
         print(f"Missing data for {current_date}")
     else:
-        highs.append(high)
-        lows.append(low)
-        dates.append(current_date)
+        highsD.append(highD)
+        lowsD.append(lowD)
+        datesD.append(current_dateD)
     
 
 print(highs)
@@ -36,15 +42,15 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 
 
-plt.plot(dates, highs, c="red", alpha= .05)
-plt.plot(dates, lows, c="blue", alpha= .05)
+plt.plot(dates, highs, c="red", alpha= 0.5)
+plt.plot(dates, lows, c="blue", alpha= 0.5)
 
 plt.title("Daily High Temp, July 2018", fontsize=16)
 plt.xlabel("")
 plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis="both", lablesize=16)
+plt.tick_params(axis="both", labelsize=16)
 
-plt.fill_between(dates, highs, lows, facecolor= "blue",alpha= .01)
+plt.fill_between(dates, highs, lows, facecolor= "blue",alpha= 0.1)
 
 fig.autofmt_xdate()
 
